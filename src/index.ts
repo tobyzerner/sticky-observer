40e4a2f1055179232ed8132ac2b4e5f8f0190f66
Toby Zerner <toby.zerner@gmail.com>
2023-11-25 08:28:19 +1030

    fix: take the last IntersectionObserverEntry

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,13 @@ export default class StickyObserver {
     private readonly el: Element;
     private readonly callback: (stuck: boolean, el: Element) => void;
     private readonly options: StickyObserverOptions;
-    private observer: IntersectionObserver;
+    private observer?: IntersectionObserver;
 
-    constructor(el: Element, callback: (stuck: boolean, el: Element) => void, options: StickyObserverOptions = {}) {
+    constructor(
+        el: Element,
+        callback: (stuck: boolean, el: Element) => void,
+        options: StickyObserverOptions = {},
+    ) {
         this.el = el;
         this.callback = callback;
         this.options = options;
@@ -23,17 +27,27 @@ export default class StickyObserver {
         // For sides that the element doesn't stick to, use 100% so
         // that the threshold will never be reached on that side.
         const style = getComputedStyle(this.el);
-        const rootMargin = ['top', 'right', 'bottom', 'left']
-            .map(side => style[side] === 'auto' ? '100%' : `${-1 * parseInt(style[side]) - 1}px`)
+        const rootMargin = (['top', 'right', 'bottom', 'left'] as const)
+            .map((side) =>
+                style[side] === 'auto'
+                    ? '100%'
+                    : `${-1 * parseInt(style[side]) - 1}px`,
+            )
             .join(' ');
 
-        this.observer = new IntersectionObserver(entries => {
-            this.callback(! entries[0].isIntersecting, this.el);
-        }, {
-            threshold: [1],
-            rootMargin,
-            root: this.options.root,
-        });
+        this.observer = new IntersectionObserver(
+            (entries) => {
+                this.callback(
+                    !entries[entries.length - 1].isIntersecting,
+                    this.el,
+                );
+            },
+            {
+                threshold: [1],
+                rootMargin,
+                root: this.options.root,
+            },
+        );
 
         this.observer.observe(this.el);
     }
